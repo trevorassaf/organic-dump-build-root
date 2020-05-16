7d5b435b05040c6f30a364990b6743f35bc75418
Trevor Assaf <bozkurtus@google.com>
2020-05-16 14:43:17 -0700

    Fix find pis script

--- scripts/find-pis-on-local-network.sh
+++ scripts/find-pis-on-local-network.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sudo nmap -sS -p 22 192.168.1.0/24
+ssid_info=$(sudo nmap -sS -p 22 192.168.1.0/24)
+echo $ssid_info | sed "s/Nmap scan report for/\r\nNmap scan report for/g" | grep "Raspberry Pi Foundation"
